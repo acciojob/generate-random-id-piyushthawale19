189d294a9dafe96c834da31ecbbde632f3f90f3c
PIYUSH ARUN THAWLE <151814546+piyushthawale19@users.noreply.github.com>
2026-06-06 00:40:09 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,16 @@
 function makeid(l) {
   // write your code here
+	var res = "";
+	var char_lsit="ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789"
+
+	for(var i=0;i<l;i++){
+		res += char_lsit.charAt(
+			Math.floor(
+			Math.random()*char_lsit.length
+				)
+		)
+	}
+	return res;
 }
 
 // Do not change the code below.
